c4d342f6f52cf1b3be60915a37d52c35d95c160d
Peihong Guo <peihongguo@gmail.com>
2013-06-14 10:41:48 -0500

    10038 - Jolly Jumpers AC

--- 10038 - JollyJumpers/main.cpp
+++ 10038 - JollyJumpers/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <cmath>
+using namespace std;
 
 int main(){
+	int numbers[3000];
+	bool diffs[3000];
+	int numcount, diffcount;
+	while(true){
+		cin >> numcount;
+		if( cin.eof() ) break;
 
+		for(int i=0;i<numcount;i++)
+			cin >> numbers[i];
+
+		for(int i=0;i<numcount;i++)
+			diffs[i] = false;
+
+		diffcount = 0;
+		for(int i=0;i<numcount-1;i++){
+			int diff = abs(numbers[i+1] - numbers[i]);
+			if( diff < 0 ) diff = -diff;
+
+			if( diff > numcount) continue;
+			if( !diffs[diff] ){
+				diffcount++;
+				diffs[diff] = true;
+			}
+		}
+
+		cout << ((diffcount == numcount-1)?"Jolly":"Not jolly") << endl;
+	}
+	return 0;
 }
