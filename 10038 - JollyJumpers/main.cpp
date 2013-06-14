8bd02c17274d5543ee90598c75e2275687aeea04
Peihong Guo <peihongguo@gmail.com>
2013-06-14 10:57:24 -0500

    10038 - another attemp to optimize

--- 10038 - JollyJumpers/main.cpp
+++ 10038 - JollyJumpers/main.cpp
@@ -1,4 +1,6 @@
-#include <cstdio>
+#include <iostream>
+#include <sstream>
+#include <string>
 #include <cstdlib>
 #include <cmath>
 using namespace std;
@@ -7,26 +9,32 @@ int main(){
 	int preval, curval;
 	bool diffs[3000];
 	int numcount, diffcount;
-	while( (scanf("%d", &numcount) == 1) ){
+	string buf;
+	while( true ){
+		getline(cin, buf);
+		if( buf.empty() ) break;
+		stringstream ss;
+		ss.str(buf);
+		ss >> numcount;
 		for(int i=0;i<numcount;i++)
 			diffs[i] = false;
 
-		scanf("%d", &preval);
+		ss >> preval;
 		curval = preval;
 		diffcount = 0;
 		for(int i=1;i<numcount;i++)
 		{
 			preval = curval;
-			scanf("%d", &curval);
+			ss >> curval;
 			int diff = abs(curval - preval);
-			if( diff > numcount) continue;
+			if( diff > numcount) break;		// impossible to be jolly, no need to do more checking
 			if( !diffs[diff] ){
 				diffcount++;
 				diffs[diff] = true;
 			}
 		}
 
-		printf("%s\n",((diffcount == numcount-1)?"Jolly":"Not jolly"));
+		cout << ((diffcount == numcount-1)?"Jolly":"Not jolly") << endl;
 	}
 	return 0;
 }
