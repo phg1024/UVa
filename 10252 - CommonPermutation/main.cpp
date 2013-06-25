3743f7520acdc7e539c98aa83b7e9b0741c93ee7
Peihong Guo <peihongguo@gmail.com>
2013-06-25 15:30:58 -0500

    10252 - improved performance

--- 10252 - CommonPermutation/main.cpp
+++ 10252 - CommonPermutation/main.cpp
@@ -4,22 +4,22 @@
 #include <cstdio>
 using namespace std;
 
-void countWord(const char* s, int counter[128])
+void countWord(const string& s, int counter[128])
 {
-    int i=0;
-    char c;
-    while(( c = s[i++] ) != '\0' )
-        counter[c]++;
+    for(size_t i=0;i<s.size();i++)
+        counter[s[i]]++;
 }
 
 int main(){
     string a, b;
-    while( getline(cin, a) && getline(cin, b) )
-    {
-        int counter[2][128] = {0};
 
-        countWord(a.c_str(), counter[0]);
-        countWord(b.c_str(), counter[1]);
+    while( getline(cin, a) )
+    {
+        getline(cin, b);
+
+        int counter[2][128] = {0};
+        countWord(a, counter[0]);
+        countWord(b, counter[1]);
 
         char buf[1001]={0};
         for(int i='a', pos=0;i<='z';i++)
