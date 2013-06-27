e74a7be8c8b7d806138facb31ca5b0950ff3dd00
Peihong Guo <peihongguo@gmail.com>
2013-06-27 03:53:26 -0500

    10150 - tried to improve performance

--- 10150 - Doublets/main.cpp
+++ 10150 - Doublets/main.cpp
@@ -6,10 +6,11 @@
 #include <map>
 using namespace std;
 
-set<string> visitedWords;
-set<string> dict[17];
+set<int> visitedWords;
+map<string, int> dict[17];
+vector<string> wordSet[17];
 
-vector<string> getNeighbor(const set<string>& dict, set<string>& visitedWords, const string& word)
+vector<string> getNeighbor(const map<string,int>& dict, const vector<string>& words, set<int>& visitedWords, const string& word)
 {
 	vector<string> v;
 	for(size_t i=0;i<word.size();i++)
@@ -18,52 +19,59 @@ vector<string> getNeighbor(const set<string>& dict, set<string>& visitedWords, c
 		{
 			string str = word;
 			str[i] = j;
-			if( (dict.find(str) != dict.end())
-				&& (visitedWords.find(str) == visitedWords.end()) )
+			if( (dict.find(str) != dict.end()) )
 			{
-				visitedWords.insert( str );
-				v.push_back( str );
+				int idx = dict.at(str);
+				if( (visitedWords.find(idx) == visitedWords.end()) )
+				{
+					visitedWords.insert( idx );
+					v.push_back( str );
+				}
 			}
 		}
 	}
 	return v;
 }
 
-vector<string> findPath(const set<string>& dict, const string& a, const string& b)
+vector<string> findPath(const map<string, int>& dict, const vector<string>& words, const string& a, const string& b)
 {
 	bool found = false;
-	queue<string> Q;
-	map<string, string> parent;
-	parent[a] = "";
-	Q.push(a);
-	visitedWords.insert(a);
+	queue<int> Q;
+	map<int, int> parent;
+	int rootIdx = dict.at(a);
+	int targetIdx = dict.at(b);
+	parent[rootIdx] = -1;
+	Q.push( rootIdx );
+	visitedWords.insert( rootIdx );
 	while( !Q.empty() )
 	{
-		string word = Q.front();
-		if( word == b )
+		int wordIdx = Q.front();
+		string word = words[wordIdx];
+		if( wordIdx == targetIdx )
 		{
 			found = true;
 			break;
 		}
 		Q.pop();
 
-		vector<string> neighbors = getNeighbor(dict, visitedWords, word );
+		vector<string> neighbors = getNeighbor(dict, words, visitedWords, word );
 		for(size_t i=0;i<neighbors.size();i++)
 		{
-			parent[neighbors[i]] = word;
-			Q.push(neighbors[i]);
+			int idx = dict.at(neighbors[i]);
+			parent[idx] = wordIdx;
+			Q.push( idx );
 		}
 	}
 
 	if( found )
 	{
 		vector<string> path;
-		string w = b;
+		int w = targetIdx;
 		while(true)
 		{
-			path.push_back(w);
-			if( parent[w] == "" ) break;
-			w = parent[w];
+			path.push_back( words[w] );
+			if( parent.at(w) == -1 ) break;
+			w = parent.at(w);
 		};
 
 		return path;
@@ -80,7 +88,8 @@ int main()
 		getline(cin, buf);
 		if( buf.empty() ) break;
 
-		dict[buf.size()].insert( buf );
+		dict[buf.size()][buf] = wordSet[buf.size()].size();
+		wordSet[buf.size()].push_back(buf);
 	}
 
 	string a, b;
@@ -94,7 +103,7 @@ int main()
 		if( a.size() == b.size() )
 		{
 			visitedWords.clear();
-			vector<string> path = findPath( dict[a.size()], a, b );
+			vector<string> path = findPath( dict[a.size()], wordSet[a.size()], a, b );
 			if( !path.empty() )
 			{
 				for(vector<string>::reverse_iterator it = path.rbegin(); it!=path.rend(); it++)
