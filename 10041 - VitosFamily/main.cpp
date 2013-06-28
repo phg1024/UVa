c3fed9a6f54f9e3f885fa57a27847ca55512dbed
Peihong Guo <peihongguo@gmail.com>
2013-06-28 13:54:21 -0500

    10041 - VitosFamilty improved performance

--- 10041 - VitosFamily/main.cpp
+++ 10041 - VitosFamily/main.cpp
@@ -3,45 +3,69 @@
 #include <algorithm>
 #include <string>
 #include <cmath>
+#include <cstdio>
 using namespace std;
 
-bool comp(unsigned int a, unsigned int b)
+inline void swap(int& a, int& b)
 {
-    return a < b;
+    int tmp = a;
+    a = b;
+    b = tmp; 
 }
 
-unsigned int computeDistance(int s[500], int count, int pos)
+int partition(int s[500], int left, int right, int pivotIndex)
 {
-    unsigned int d = 0;
-    for(int i=0;i<count;i++)
+    int pivotValue = s[pivotIndex];
+    swap(s[right], s[pivotIndex]);
+
+    int storeIndex = left;
+
+    for(int i=left;i<=right-1;i++)
     {
-        d += abs( s[i] - pos );
+        if( s[i] <= pivotValue )
+        {
+            swap(s[storeIndex], s[i]);
+            storeIndex++;
+        }
     }
-    return d;
+
+    swap(s[right], s[storeIndex]);
+    return storeIndex;
+}
+
+int select(int s[500], int left, int right, int k)
+{
+    if( left == right )
+        return left;;
+
+    int pivotIndex = (left + right)/2;
+    int pivotNewIndex = partition(s, left, right, pivotIndex);
+    int pivotDist = pivotNewIndex - left;
+
+    if( pivotDist == k )
+        return pivotNewIndex;
+    else if( k < pivotDist )
+        return select(s, left, pivotNewIndex-1, k);
+    else
+        return select(s, pivotNewIndex + 1, right, k - pivotDist - 1);
 }
 
 int main()
 {
     int ncases;
     int s[500];
-    unsigned int dists[30000];
-    cin >> ncases;
+    scanf("%d", &ncases);
     for(int i=0;i<ncases;i++)
     {
         int nneighbors;
-        cin >> nneighbors;
+        scanf("%d", &nneighbors);
 
-        int maxS, minS;
-        maxS = 0, minS = 30001;
         for(int i=0;i<nneighbors;i++)
-        {
-            int t;
-            cin >> t;
-            s[i] = t;
-            if( t > maxS ) maxS = t;
-            if( t < minS ) minS = t;
-        }
+            scanf("%d", s+i);
 
+//#define SORTING_BASED
+#ifdef SORTING_BASED 
+        // sorting based method
         std::sort(s, s+nneighbors, std::greater<int>());
 
         int pos;
@@ -54,7 +78,42 @@ int main()
             pos = (int)((s[nneighbors/2-1] + s[nneighbors/2]) * 0.5 + 0.5);
         }
 
-        cout << computeDistance( s, nneighbors, pos ) << endl;
+        unsigned int d = 0;
+        for(int j=0;j<nneighbors;j++)
+        {
+            int val = s[j] - pos;
+            d += val>0?val:(-val);
+        }
+
+        printf("%d\n", d);
+#else
+        // selection based method
+        int pos;
+        unsigned int d = 0;
+        if( nneighbors % 2 == 1 )
+        {
+            int idx = select(s, 0, nneighbors-1, nneighbors/2 );
+            pos = s[idx];
+
+            for(int j=0;j<idx;j++)
+                d += pos - s[j];
+            for(int j=idx+1;j<nneighbors;j++)
+                d += s[j] - pos;
+        }
+        else
+        {
+            int left = select(s, 0, nneighbors-1, nneighbors/2-1);
+            int right = select(s, left + 1, nneighbors-1, 0) ;
+            pos = (int)(( s[left] + s[right] ) / 2.0 );
+
+            for(int j=0;j<=left;j++)
+                d += pos - s[j];
+            for(int j=left+1;j<nneighbors;j++)
+                d += s[j] - pos;
+        }
+
+        printf("%d\n", d);
+#endif
     }
     return 0;
 }
