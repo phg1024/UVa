7b2b220469a87d7734b28eabc6ea70b076d3a85a
Peihong Guo <peihongguo@gmail.com>
2013-06-28 17:34:20 -0500

    120 - improved performance

--- 120 - StacksOfFlapJacks/main.cpp
+++ 120 - StacksOfFlapJacks/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -8,7 +9,18 @@ using namespace std;
 
 struct Pancake
 {
-    bool operator==(const Pancake& p) const
+    Pancake& operator=(const Pancake& p)
+    {
+        if( this != &p )
+        {
+            this->r = p.r;
+            this->id = p.id;
+            this->order = p.order;
+        }
+        return (*this);
+    }
+
+   bool operator==(const Pancake& p) const
     {
         return (id == p.id);
     }
@@ -34,16 +46,6 @@ struct Pancake
         return ((*this) < p) || ((*this) == p);
     }
 
-    Pancake& operator=(const Pancake& p)
-    {
-        if( this != &p )
-        {
-            this->r = p.r;
-            this->id = p.id;
-        }
-        return (*this);
-    }
-
     friend ostream& operator<<(ostream& os, const Pancake& p)
     {
         os << "(" << p.r << ", " << p.id << ")";
@@ -51,45 +53,16 @@ struct Pancake
     }
     int r;
     int id;
+    int order;
 };
-
-inline bool inorder( const vector<Pancake>& v )
-{
-    for(int i=0;i<v.size()-1;i++)
-        if( v[i] > v[i+1] )
-            return false;
-    
-    return true;
-}
-
-vector<Pancake> flip( const vector<Pancake>& v, int pos )
-{
-    int actualPos = pos - 1;
-    /*
-    cout << "flipping " << pos << endl;
-    cout << "actual pos " << actualPos << endl;
-    */
-
-    vector<Pancake> result;
-    for(int i=0;i<=actualPos;i++)
-    {
-        result.push_back(v[i]);
-    }
-
-    std::reverse(result.begin(), result.end());
-
-    for(int i=actualPos+1;i<v.size();i++)
-        result.push_back(v[i]);
-
-    return result;
-}
 
 int main()
 {
     string buf;
     while( getline(cin, buf) )
     {
-        vector<Pancake> sequence;
+        Pancake sequence[30];
+        Pancake sortedSequence[30];
 
         stringstream ss;
         ss << buf;
@@ -99,35 +72,28 @@ int main()
         {
             Pancake p;
             p.r = x;
-            p.id = count++;
-            sequence.push_back(p);
+            p.id = count;
+            sequence[count] = p;
+            sortedSequence[count] = p;
+            count++;
         }
 
-
         // output the original sequence
-        for(int i=0;i<sequence.size();i++)
-            cout << sequence[i].r << ' ';
-        cout << endl;
+        for(int i=0;i<count;i++)
+            printf("%d ", sequence[i].r);
+        putchar('\n');
 
-        vector<Pancake> sortedSequence = sequence;
-        std::sort(sortedSequence.begin(), sortedSequence.end(), std::less<Pancake>());
-        /*
-        for(int i=0;i<sequence.size();i++)
-            cout << sortedSequence[i] << ' ';
-        cout << endl;
-        */
+        std::sort(sortedSequence, sortedSequence+count, std::less<Pancake>());
 
-
-        map<Pancake, int> positionMap;
-        for(int i=0;i<sortedSequence.size();i++)
-            positionMap[sortedSequence[i]] = i;
+        for(int i=0;i<count;i++)
+            sequence[sortedSequence[i].id].order = i;
 
         vector<int> operations;
-        while( !inorder(sequence) )
+        while( true )
         {
             // search for the first out-of-order item
-            int pos = 0;
-            for(int i=0;i<sequence.size() - 1;i++)
+            int pos = -1;
+            for(int i=0;i<count - 1;i++)
             {
                 if( sequence[i] > sequence[i+1] )
                 {
@@ -136,48 +102,29 @@ int main()
                 }
             }
 
-            // locate its actual position
-            int targetPos = positionMap.at(sequence[pos]);
+            if( pos == -1 ) break;
 
             // flip to top
-            sequence = flip( sequence, pos+1);
-            
-            /*
-            for(int i=0;i<sequence.size();i++)
-                cout << sequence[i]<< ' ';
-            cout << endl;
-            */
-            
-
             if( pos != 0 )
             {
-                operations.push_back(sequence.size() - pos);
+                std::reverse(sequence, sequence+pos+1);
+                operations.push_back(count - pos);
             }
 
+            // take the top item
+            int targetPos = sequence[0].order;
             // flip to the right place
-
-            sequence = flip( sequence, targetPos+1);
-           
-            /*
-            for(int i=0;i<sequence.size();i++)
-                cout << sequence[i] << ' ';
-            cout << endl;
-            */
-          
             if( targetPos != 0 )
-                operations.push_back(sequence.size()-targetPos);
+            {
+                std::reverse(sequence, sequence+targetPos+1);
+                operations.push_back(count-targetPos);
+            }
         }
-
-        /*
-        for(int i=0;i<sequence.size();i++)
-            cout << sequence[i].r << ' ';
-        cout << endl;
-        */
 
         operations.push_back(0);
         for(int i=0;i<operations.size();i++)
-            cout << operations[i] << ' ';
-        cout << endl;
+            printf("%d ", operations[i]);
+        putchar('\n');
     }
     return 0;
 }
