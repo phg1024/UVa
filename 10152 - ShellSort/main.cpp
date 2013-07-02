76e0a551066e86a4a849c5ff0846661fa7b045cd
Peihong Guo <peihongguo@gmail.com>
2013-07-02 16:32:19 -0500

    10152 - improved performance

--- 10152 - ShellSort/main.cpp
+++ 10152 - ShellSort/main.cpp
@@ -1,20 +1,20 @@
-#include <cstring>
-#include <cstdio>
-#include <cstdlib>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
 	int ncases;
     scanf("%d\n", &ncases);
 	
+    char src[256][128] = {0};
+    char dst[256][128] = {0};
+
 	while( ncases-- > 0 )
 	{
 		int n;
         scanf("%d\n", &n);
 
-        char src[200][128] = {0};
-        char dst[200][128] = {0};
 
 		for(int i=0;i<n;i++)
 		{
