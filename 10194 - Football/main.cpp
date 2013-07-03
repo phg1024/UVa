8ee69147bfe372941139152ce9844a1bfd5d0563
Peihong Guo <peihongguo@gmail.com>
2013-07-03 15:47:31 -0500

    10194 - improved performance

--- 10194 - Football/main.cpp
+++ 10194 - Football/main.cpp
@@ -1,22 +1,32 @@
-#include <iostream>
-#include <string>
 #include <cstdio>
 #include <algorithm>
-#include <map>
+#include <functional>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
 using namespace std;
 
-string lower(const string& str)
+bool comp(const char* str1, const char* str2)
 {
-    string s = str;
-    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
-    return s;
+    int L1 = strlen(str1);
+    int L2 = strlen(str2);
+    int L = min(L1, L2);
+    for(int i=0;i<L;i++)
+    {
+        if( tolower(str1[i]) != tolower(str2[i]) )
+            return tolower(str1[i]) < tolower(str2[i]);
+    }
+
+    return L1 < L2;
 }
 
 struct Team
 {
     Team():points(0), wins(0), loses(0), ties(0), 
-           goalsScored(0), goalsAgainst(0), games(0),
-           name(""){}
+           goalsScored(0), goalsAgainst(0), games(0)
+           {
+               memset(name, 0, sizeof(char)*32);
+           }
     bool operator>(const Team& t) const
     {
         if( points > t.points ) return true;
@@ -33,7 +43,7 @@ struct Team
                     {
                         if( games < t.games ) return true;
                         else if( games == t.games) 
-                            return lower(name) < lower(t.name);
+                            return comp(name, t.name);
                     }
                 }
             }
@@ -48,60 +58,67 @@ struct Team
     int goalsAgainst;
     int goalsDiff;
     int games;
-    string name;
+    char name[32];
 };
 
-void parse(const string& str, string name[], int goals[])
+void parse(const char* str, char name[][32], int goals[])
 {
-    int pounds[2];
-    pounds[0] = str.find_first_of('#');
-    pounds[1] = str.find_last_of('#');
-    name[0] = str.substr(0, pounds[0]);
-    name[1] = str.substr(pounds[1]+1);
-    string scores = str.substr(pounds[0]+1, pounds[1]-pounds[0]-1);
+    int pounds[2] = {0};
+    pounds[0] = strchr(str, '#') - str;
+    pounds[1] = strchr(str+pounds[0]+1, '#') - str;
+
+    strncpy( name[0], str, pounds[0] );
+    strncpy( name[1], str + pounds[1] + 1, strlen(str) - pounds[1] );
     char dummy;
-    sscanf(scores.c_str(),"%d%c%d", &(goals[0]), &dummy, &(goals[1]));
+    sscanf(str+pounds[0]+1,"%d%c%d", &(goals[0]), &dummy, &(goals[1]));
+}
+
+int teamIndex( Team teams[], int nTeams, char* name )
+{
+    for(int i=0;i<nTeams;i++)
+        if( strcmp( teams[i].name, name ) == 0 )
+        {
+            return i;
+        }
+    return -1;
 }
 
 int main()
 {
     int nTournaments;
-    cin >> nTournaments;
-    cin.ignore();
+    scanf("%d\n", &nTournaments);
 
     while( nTournaments > 0 )
     {
-        string tournamentName;
-        getline(cin, tournamentName);
+        char tournamentName[128];
+        fgets(tournamentName, 128, stdin);
 
         int nTeams;
-        cin >> nTeams;
-        cin.ignore();
+        scanf("%d\n", &nTeams);
         
         Team teams[30];
-
-        map<string, int> order;
         for(int i=0;i<nTeams;i++)
         {
-            getline(cin, teams[i].name);
-            order[teams[i].name] = i;
+            fgets(teams[i].name, 32, stdin);
+            teams[i].name[strlen(teams[i].name)-1] = '\0';
         }
 
         int nGames;
-        cin >> nGames;
-        cin.ignore();
-        string buf;
+        scanf("%d\n", &nGames);
+
+        char buf[128];
         for(int i=0;i<nGames;i++)
         {
-            string name[2];
+            char name[2][32] = {0};
             int goals[2];
 
-            getline(cin, buf);
+            fgets(buf, 128, stdin);
+            buf[strlen(buf)-1] = '\0';
             parse(buf, name, goals);
 
             int idx[2];
-            idx[0] = order.at(name[0]);
-            idx[1] = order.at(name[1]);
+            idx[0] = teamIndex(teams, nTeams, name[0]);
+            idx[1] = teamIndex(teams, nTeams, name[1]);
             Team& team0 = teams[idx[0]];
             Team& team1 = teams[idx[1]];
 
@@ -142,16 +159,16 @@ int main()
 
         std::sort(teams, teams+nTeams, std::greater<Team>());
 
-        cout << tournamentName << endl;
+        printf("%s", tournamentName);
         for(int i=0;i<nTeams;i++)
         {
             printf("%d) %s %dp, %dg (%d-%d-%d), %dgd (%d-%d)\n",
-                   i+1, teams[i].name.c_str(), teams[i].points, teams[i].games,
+                   i+1, teams[i].name, teams[i].points, teams[i].games,
                    teams[i].wins, teams[i].ties, teams[i].loses,
                    teams[i].goalsDiff, teams[i].goalsScored, teams[i].goalsAgainst );
         }
 
-        if( -- nTournaments != 0 ) cout << endl; 
+        if( -- nTournaments != 0 ) putchar('\n'); 
     }
     return 0;
 }
