89d1b020c315471d02f1f3aa16219ebbc18fbf25
phgdragon <peihongguo@gmail.com>
2013-07-04 03:43:50 -0500

    10035 - simplified code

--- 10035 - PrimaryArithmetic/main.cpp
+++ 10035 - PrimaryArithmetic/main.cpp
@@ -8,60 +8,33 @@
 #include <algorithm>
 using namespace std;
 
-int add_bignum(char a[], char b[])
+int add(char a[], char b[])
 {
     int L1 = strlen(a);
     int L2 = strlen(b);
     std::reverse(a, a+L1);
     std::reverse(b, b+L2);
 
-    int L = min(L1, L2);
+    for(int i=0;i<L1;i++)
+        a[i] -= '0';
+
+    for(int i=0;i<L2;i++)
+        b[i] -= '0';
+
+    int L = max(L1, L2);
 
     char c[12] = {0};
+    memcpy(c, a, sizeof(char)*L1);
     int carry = 0;
     for(int i=0;i<L;i++)
     {
-        int val = (a[i] - '0') + (b[i] - '0') + c[i];
+        int val = b[i] + c[i];
         if( val >= 10 )
         {
             carry++;
-            c[i] = val-10;
             c[i+1]++;
         }
-        else
-            c[i] = val;
     }
-    if( L1 > L2 )
-    {
-        for(int i=L;i<L1;i++)
-        {
-            int val = (a[i] - '0') + c[i];
-            if( val >= 10 )
-            {
-                carry++;
-                c[i] = val - 10;
-                c[i+1]++;
-            }
-            else
-                c[i] = val;
-        }
-    }
-    else
-    {
-        for(int i=L;i<L2;i++)
-        {
-            int val = (b[i] - '0') + c[i];
-            if( val >= 10 )
-            {
-                carry++;
-                c[i] = val - 10;
-                c[i+1]++;
-            }
-            else
-                c[i] = val;
-        }
-    }
-
 
     return carry;
 }
@@ -69,14 +42,14 @@ int add_bignum(char a[], char b[])
 int main(){
     while( true )
     {
-        char a[11], b[11];
+        char a[11] = {0}, b[11] = {0};
         scanf("%s %s\n", a, b);
 
         if( a[0] == '0' && b[0] == '0' )
             break;
         else
         {
-            int carry = add_bignum(a, b);
+            int carry = add(a, b);
 
             switch( carry )
             {
