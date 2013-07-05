531b10c383a7a58210630c9fef18ec48412df025
Peihong Guo <peihongguo@gmail.com>
2013-07-04 19:13:57 -0500

    701 - improved performance

--- 701 - TheArcheologistsDilemma/main.cpp
+++ 701 - TheArcheologistsDilemma/main.cpp
@@ -13,15 +13,18 @@ using namespace std;
 
 int findExp_fast(unsigned int num)
 {
+    // base = log2(10.0)
     const double base = 3.321928094887362181708567732130177319049835205078125;
 	double lognum = log2(num);
 	double lognum1 = log2(num+1);
-	int L = floor(log10(num)) + 1;
+    // length of the number
+	int L = floor( log10(num) ) + 1;
 	
     int k = L + 1;
     while( true )
     {
         double diff = k * base;
+
         unsigned int a = floor( lognum + diff ); 
         unsigned int b = floor( lognum1 + diff ); 
         if( b > a )
@@ -35,11 +38,7 @@ int main()
     unsigned int num;
 	while (scanf("%u", &num) != EOF) {
 		int idx = findExp_fast(num);
-		if (idx) {
-			printf("%d\n", idx);
-		} else {
-			printf("no power of 2\n");
-		}
+		printf("%d\n", idx);
 	}
 	return 0;
 }
