91c8aa10624995cb44145f08dca3eb7fe364ce52
phgdragon <peihongguo@gmail.com>
2013-07-06 12:43:48 -0500

    10105 Polynomial Coefficients simplified code

--- 10105 - PolynomialCoefficients/main.cpp
+++ 10105 - PolynomialCoefficients/main.cpp
@@ -21,34 +21,23 @@ int combinations[][13] =
     {1,    9,   36,   84,  126,  126,   84,   36,    9,    1,    0,    0,    0},
     {1,   10,   45,  120,  210,  252,  210,  120,   45,   10,    1,    0,    0},
     {1,   11,   55,  165,  330,  462,  462,  330,  165,   55,   11,    1,    0},
-    {1,   12,   66,  220,  495,  792,  924,  792,  495,  220,   66,   12,    1},
+    {1,   12,   66,  220,  495,  792,  924,  792,  495,  220,   66,   12,    1}
 };
-
-int computeCoeffs_fast(int n, int k, int *exponent)
-{
-	int val = 1;
-	for(int i=0;i<k;i++)
-	{
-		val *= combinations[n][exponent[i]];
-		n -= exponent[i];
-	}
-	return val;
-}
 
 int main()
 {
     int n, k;
     while( scanf("%d %d", &n, &k) != EOF )
     {
-        int exponent[12];
+		int coeff = 1;
         for(int i=0;i<k;i++)
         {
-            scanf("%d", &exponent[i]);
+	        int e;
+            scanf("%d", &e);
+			coeff *= combinations[n][e];
+			n -= e;
         }
-
-		int coeff2 = computeCoeffs_fast(n, k, exponent);
-
-        printf("%d\n", coeff2);
+        printf("%d\n", coeff);
     }
     return 0;
 }
