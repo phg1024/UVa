53ce36f32fd18c7c0da63766d2d99235179f62ef
Peihong Guo <peihongguo@gmail.com>
2013-07-15 18:36:08 -0500

    10049 - Self-describing Sequence in progress

---
+++ 10049 - Self-describing Sequence/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <functional>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+using namespace std;
+
+int main(){
+    return 0;
+}
+
