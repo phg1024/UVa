db4fa5eeb1f2901e6ed25d64c817bc275e2443b2
phgdragon <peihongguo@gmail.com>
2013-07-18 03:12:47 -0500

    10110 - Light more light improved performance

--- 10110 - Light more light/main.cpp
+++ 10110 - Light more light/main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <functional>
 #include <vector>
+#include <bitset>
 #include <map>
 
 #include <cstring>
@@ -10,81 +11,82 @@
 #include <cstdlib>
 #include <cmath>
 using namespace std;
+#define FAST 1	
 
+#if !FAST
 int primes[6543];
 int primeCount;
 
 void init()
 {
-	char flag[65539] = {0};
-	
-	for(int i=2;i<258;i++)
+	bitset<65536> flag(0);
+	for(int i=2;i<257;)
 	{
-		for(int j=i+i;j<65539;j+=i)
+		for(int j=i+i;j<65536;j+=i)
 			flag[j] = 1;
+		
+		i++;
+		while( flag[i] == 1 && i < 257 ) i++;
 	}
-
+	
 	primeCount = 0;	
-	for(int i=2;i<65539;i++)
+	for(int i=2;i<65536;)
 	{
 		if( flag[i] == 0 )
 			primes[primeCount++] = i;
+		
+		i++;
+		while( flag[i] == 1 && i < 65536 ) i++;
 	}
 }
+#endif
 
-int divisibleCount(unsigned int n)
-{		
-	char facs[16] = {0};
-	int facCount = 0;
+bool divisibleCount(unsigned int n)
+{
+#if FAST
+	// note that the return value is false if any of the factors of n have odd counts
+	// otherwise the return value is true, i.e. all the factors have even counts
 	
+	// the only possibility that all the factors of n have even counts is that n itself
+	// is a perfect squre
+	unsigned int s = sqrt(n);
+	return (s * s == n);
+#else
 	for(int i=0;i<primeCount;i++)
 	{
 		int p = primes[i];
 		if( p * p > n ) break;
 		
 		if( n % p == 0 )
-		{
+		{		
+			int count = 0;
 			while( n % p == 0 )
 			{
-				facs[facCount]++;
+				count++;
 				n /= p;
 			}
 			
-			// hack, return value would be even if facs[facCount] is odd
-			if( facs[facCount] & 0x1 ) return 0;
-			facCount++;
+			if( count & 0x1 ) return false;
 		}		
 	}
 	
 	if( n != 1 )
-	{
-		// hack, return value would be even if facs[facCount] is odd
-		return 0;
-		//facs[facCount++] = 1;
-	}
+		return false;
 	
-	// hack, in this case, the return value must be odd
-	return 1;
-	
-	// number of possible factor combinations
-	unsigned int count = 1;
-	for(int i=0;i<facCount;i++)
-	{
-		count *= (facs[i] + 1);
-	}	
-
-	return count;
+	return true;
+#endif
 }
 
 int main()
 {
+#if !FAST
 	init();
-
-	unsigned int n = 2;
+#endif
+	unsigned int n;
 	while( scanf("%u", &n) )
 	{
 		if( n == 0 ) break;
-		printf("%s\n", ((divisibleCount(n) & 0x1)?"yes":"no") );
+		printf("%s\n", ((divisibleCount(n))?"yes":"no") );
 	}
     return 0;
 }
