d6b02b790855f34ff853032832b025d0c731cf7a
phgdragon <peihongguo@gmail.com>
2013-07-18 02:28:41 -0500

    10110 - Light more light improved performance

--- 10110 - Light more light/main.cpp
+++ 10110 - Light more light/main.cpp
@@ -11,82 +11,80 @@
 #include <cmath>
 using namespace std;
 
-int primes[6544];
+int primes[6543];
 int primeCount;
-
-bool isPrime(unsigned int n)
-{
-	for(int i=0;i<primeCount;i++)
-	{
-		if( n == primes[i] ) return true;
-		if( n % primes[i] == 0 ) return false;
-	}
-	
-	return true;
-}
 
 void init()
 {
-	primes[0] = 2;
-	primeCount = 1;
-	for(int i=3;i<65538;i+=2)
+	char flag[65539] = {0};
+	
+	for(int i=2;i<258;i++)
 	{
-		if( isPrime(i) )
-		{
+		for(int j=i+i;j<65539;j+=i)
+			flag[j] = 1;
+	}
+
+	primeCount = 0;	
+	for(int i=2;i<65539;i++)
+	{
+		if( flag[i] == 0 )
 			primes[primeCount++] = i;
-		}
 	}
 }
 
 int divisibleCount(unsigned int n)
-{
-	unsigned int n0 = n;
-	if( n == 1 ) return 1;
-	
-	if( isPrime(n) ) return 2;
-	
-	vector<int> facs;
-	vector<unsigned int> comps;
+{		
+	char facs[16] = {0};
+	int facCount = 0;
 	
 	for(int i=0;i<primeCount;i++)
 	{
 		int p = primes[i];
-		if( p > n ) break;
+		if( p * p > n ) break;
+		
 		if( n % p == 0 )
 		{
-			facs.push_back(0);
-			comps.push_back(p);
 			while( n % p == 0 )
 			{
-				facs.back()++;
+				facs[facCount]++;
 				n /= p;
 			}
+			
+			// hack, return value would be even if facs[facCount] is odd
+			if( facs[facCount] & 0x1 ) return 0;
+			facCount++;
 		}		
 	}
 	
 	if( n != 1 )
 	{
-		facs.push_back(1);
-		comps.push_back(n);
+		// hack, return value would be even if facs[facCount] is odd
+		return 0;
+		//facs[facCount++] = 1;
 	}
 	
+	// hack, in this case, the return value must be odd
+	return 1;
+	
+	// number of possible factor combinations
 	unsigned int count = 1;
-	for(size_t i=0;i<facs.size();i++)
+	for(int i=0;i<facCount;i++)
 	{
 		count *= (facs[i] + 1);
 	}	
-		
+
 	return count;
 }
 
 int main()
 {
 	init();
-	unsigned int n;
-	while( cin >> n )
+
+	unsigned int n = 2;
+	while( scanf("%u", &n) )
 	{
 		if( n == 0 ) break;
-		cout << ((divisibleCount(n) & 0x1)?"yes":"no") << endl;
+		printf("%s\n", ((divisibleCount(n) & 0x1)?"yes":"no") );
 	}
     return 0;
 }
