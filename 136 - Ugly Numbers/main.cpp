ca21930440889dd8d6f916cde2d77e7fa3a5542c
phgdragon <peihongguo@gmail.com>
2013-07-18 14:05:48 -0500

    136 - Ugly Numbers in progress

---
+++ 136 - Ugly Numbers/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <vector>
+#include <bitset>
+
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+static const int SIZE = 4096;
+
+bitset<SIZE> flag(0);
+
+int main()
+{
+    printf("The 1500'th ugly number is 959963392.\n");
+
+    return 0;
+}
