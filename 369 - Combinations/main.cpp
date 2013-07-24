40d21a78d78e4cf5f1720afe71f9dd43e002e3ae
phgdragon <peihongguo@gmail.com>
2013-07-24 01:43:28 -0700

    369 - Combinations added dynamic programming code

--- 369 - Combinations/main.cpp
+++ 369 - Combinations/main.cpp
@@ -14,6 +14,34 @@ using namespace std;
 // algorithm: factorize the number, then compute the result
 // alternative: dynamic programming, C(n, k) = C(n-1, k-1) + C(n-1, k)
 
+/*
+#include <cstdio>
+using namespace std;
+
+long long a[101][101];
+
+int main() {
+	int n, m;
+	a[1][1] = 1;
+	a[1][0] = 1;
+
+	for (int i = 2; i <= 100; i++) {
+		a[i][0] = 1;
+		for (int j = 1; j <= i; j++) {
+			a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
+		}
+	}
+
+	while (scanf("%d%d", &n, &m), n || m) {
+		printf("%d things taken %d at a time is %lld exactly.\n", n, m,
+				a[n][m]);
+	}
+
+	return 0;
+}
+*/
+
+
 struct fact_t
 {
     fact_t(){}
