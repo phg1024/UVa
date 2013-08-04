feb861ebdd734659483f3f882210d7d41b723b1c
peihong <phg@tamu.edu>
2013-08-04 06:03:09 -0500

    10001 - Garden of Eden structural change

--- 10001 - Garden of Eden/main.cpp
+++ 10001 - Garden of Eden/main.cpp
@@ -15,20 +15,22 @@ char tmap[8][3] = { {0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {0, 1, 1},
 
 bool hasAncestor(const bitset<8>& automata, int cellidx, int ncells, const bitset<32>& state, const bitset<32>& prevstate)
 {
-	//cout << cellidx << '\t' << prevstate <<endl;
-	if( cellidx == 0 )
+	if( cellidx < ncells - 2 )
 	{
 		for(int i=0;i<8;i++)
 		{
-			// if the first digit of the state sequence is the mapped value of the given automata
+			// if the cellidx-th digit of the state sequence is the mapped value of the given automata
 			// start the search for the rest of the state sequence
 			if( state[cellidx] == automata[i] )
 			{
-				bitset<32> ps;
-				ps[0] = tmap[i][1];
-				ps[1] = tmap[i][2];
-				ps[ncells-1] = tmap[i][0];
-				if( hasAncestor(automata, cellidx+1, ncells, state, ps) ) return true;
+				// satisfy the digit preceding current digit
+				if( prevstate[cellidx-1] == tmap[i][0] && prevstate[cellidx] == tmap[i][1] )
+				{
+					bitset<32> ps = prevstate;
+					ps[cellidx+1] = tmap[i][2];
+					if( hasAncestor(automata, cellidx+1, ncells, state, ps) )
+						return true;
+				}
 			}
 		}
 		return false;
@@ -49,7 +51,7 @@ bool hasAncestor(const bitset<8>& automata, int cellidx, int ncells, const bitse
 			}
         return false; 
 	}
-	else if( cellidx == ncells-1 )
+	else
 	{
 		// simply check if current state is satisfiable
 		for (int i = 0; i < 8; i++)  
@@ -65,26 +67,6 @@ bool hasAncestor(const bitset<8>& automata, int cellidx, int ncells, const bitse
 				}  
 			}
         return false; 
-	}
-	else
-	{
-		for(int i=0;i<8;i++)
-		{
-			// if the cellidx-th digit of the state sequence is the mapped value of the given automata
-			// start the search for the rest of the state sequence
-			if( state[cellidx] == automata[i] )
-			{
-				// satisfy the digit preceding current digit
-				if( prevstate[cellidx-1] == tmap[i][0] && prevstate[cellidx] == tmap[i][1] )
-				{
-					bitset<32> ps = prevstate;
-					ps[cellidx+1] = tmap[i][2];
-					if( hasAncestor(automata, cellidx+1, ncells, state, ps) )
-						return true;
-				}
-			}
-		}
-		return false;
 	}
 }
 
@@ -103,7 +85,26 @@ int main()
 		bitset<32> state(str);
 		//cout << automata << '\t' << ncells << '\t' << state << endl;
 
-		if( hasAncestor(automata, 0, ncells, state, bitset<32>()) )  
+		bool found = false;
+		for(int i=0;i<8;i++)
+		{
+			// if the first digit of the state sequence is the mapped value of the given automata
+			// start the search for the rest of the state sequence
+			if( state[0] == automata[i] )
+			{
+				bitset<32> ps;
+				ps[0] = tmap[i][1];
+				ps[1] = tmap[i][2];
+				ps[ncells-1] = tmap[i][0];
+				if( hasAncestor(automata, 1, ncells, state, ps) )
+				{
+					found = true;
+					break;
+				}
+			}
+		}
+
+		if( found )  
             printf("REACHABLE\n");  
         else  
             printf("GARDEN OF EDEN\n");
