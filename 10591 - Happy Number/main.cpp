06e9226cbc5eb357edbe8aab4380c38af09b836b
peihong <phg@tamu.edu>
2013-08-04 21:51:15 -0500

    10591 - Happy Number improved performance

--- 10591 - Happy Number/main.cpp
+++ 10591 - Happy Number/main.cpp
@@ -1,23 +1,32 @@
 #include<cstdio>
 #include<set>
-using std::set;
 
-int isHappy(int v, const set<int>& visited)
+static const int MAXN = 768;
+
+int next(int v)
 {
-	if( v == 1 ) return 0;
-	if( visited.find( v ) != visited.end() ) return 1;
-	else
-	{	
-		set<int> s = visited;
-		s.insert(v);
-		int x = 0;
-		while( v )
-		{
-			int d = v % 10;
-			x += d * d;
-			v /= 10;
-		}
-		return isHappy( x, s );
+	int x = 0;
+	while( v )
+	{
+		int d = v % 10;
+		x += d * d;
+		v /= 10;
+	}
+	return x;
+}
+
+int isHappy(int v)
+{
+	int visited[MAXN] = {0};
+	// bring down the number
+	v = next(v);
+	
+	while( true )
+	{
+		if( v == 1 || visited[v] ) return visited[v];
+
+		visited[v] = 1;
+		v = next(v);
 	}
 }
 
@@ -30,7 +39,7 @@ int main()
 	while( ncases-- > 0 )
 	{
 		scanf("%d", &n);
-		printf("Case #%d: %d is %s.\n", ++caseid, n, str[ isHappy(n, set<int>()) ] );
+		printf("Case #%d: %d is %s.\n", ++caseid, n, str[ isHappy(n) ] );
 	}
 	return 0;
 }
