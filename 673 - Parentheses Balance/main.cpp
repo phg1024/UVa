3c685d480cfde9a0bbb2aa08d0f4c34f58c78bc6
phgdragon <peihongguo@gmail.com>
2013-08-07 02:39:32 -0500

    673 - Parentheses Balance used stack. improved performance

--- 673 - Parentheses Balance/main.cpp
+++ 673 - Parentheses Balance/main.cpp
@@ -6,26 +6,6 @@
 using namespace std;
 
 static const int MAXL = 130;
-bool isvalid( char str[], int L )
-{
-	if( L & 0x1 ) return false;
-	if( L == 0 ) return true;
-	char s[MAXL] = {0};
-	int i=0, j=0;
-	while( i < L )
-	{
-		if( i < L - 1 )
-		{
-			if( str[i] == '(' && str[i+1] == ')' ){ i+=2; continue;}		
-			if( str[i] == '[' && str[i+1] == ']' ){ i+=2; continue;}
-		}
-		s[j++] = str[i++];
-	}
-
-	int Ls = strlen( s );
-	if( Ls == L ) return false;
-	return isvalid( s, Ls );
-}
 
 int main()
 {
@@ -34,11 +14,17 @@ int main()
 
 	while( n-- > 0 )
 	{
-		char str[MAXL] = {0};
-		fgets(str, MAXL, stdin);
-		int L = strlen(str);
-		if( str[L-1] == '\n' ) str[--L] = '\0';
-		printf("%s\n", (isvalid( str, L )?"Yes":"No"));
+		char S[MAXL] = {0};
+        int pos = 0;
+        char c;
+
+        while( (c = getchar()) != '\n' )
+        {
+            if( (c == ')' && S[pos-1] == '(') || (c == ']' && S[pos-1] == '[') ) { pos--; continue; }
+            S[pos++] = c;
+        }
+
+		printf("%s\n", (pos==0?"Yes":"No"));
 	}
 	return 0;
 }
